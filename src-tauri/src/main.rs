416e3930d47420bc3d7b675b8bf6441dac5eca3e
PeterDing <dfhayst@gmail.com>
2023-12-27 22:41:44 +0800

    Set normal window

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -53,12 +53,12 @@ fn main() {
             }
         })
         .on_window_event(|event| match event.event() {
-            tauri::WindowEvent::Focused(is_focused) => {
-                // detect click outside of the focused window and hide the app
-                if !is_focused {
-                    event.window().hide().unwrap();
-                }
-            }
+            // tauri::WindowEvent::Focused(is_focused) => {
+            //     // detect click outside of the focused window and hide the app
+            //     if !is_focused {
+            //         event.window().hide().unwrap();
+            //     }
+            // }
             tauri::WindowEvent::CloseRequested { api, .. } => {
                 if cfg!(target_os = "macos") {
                     // on macos, we need to prevent the window from closing
